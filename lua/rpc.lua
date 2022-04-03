1bd391c40a500ae855f4572a88f4d8cfb9e5d42a
m4cey <h4mmer7ime@gmail.com>
2022-04-03 15:29:28 +0000

    rpc

--- lua/rpc.lua
+++ lua/rpc.lua
@@ -1,15 +1,15 @@
 require("presence"):setup({
-    neovim_image_text   = "Notepad but with colors🌈",
+    neovim_image_text   = "like notepad but with colors 🌈",
     main_image          = "file",
     enable_line_number  = true,
     blacklist           = {},
     buttons             = false,
     -- Rich Presence text options
-    editing_text        = "Editing %s",
-    file_explorer_text  = "Browsing %s",
-    git_commit_text     = "Committing changes",
-    plugin_manager_text = "Managing plugins",
-    reading_text        = "Reading %s",
+    editing_text        = "👩‍💻 Editing %s",
+    file_explorer_text  = "📁 Browsing %s",
+    git_commit_text     = "✍  Committing changes",
+    plugin_manager_text = "🧹 Managing plugins",
+    reading_text        = "🤓 Reading %s",
     workspace_text      = "Working on %s",
     line_number_text    = "Line %s out of %s",
 })
